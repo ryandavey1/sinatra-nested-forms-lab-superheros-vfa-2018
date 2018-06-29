0ddb6a45ed087e5df490e5c968bf0c41b2615883
Ryan Davey <ryancdavey@icloud.com>
2018-06-29 08:20:17 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
-require 'sinatra/base'
+# require '../../config/environment'
 
 class App < Sinatra::Base
 
-    set :views, Proc.new { File.join(root, "../views/") }
-
-    get '/' do
+  set :views, Proc.new { File.join(root, "../views/") }
+  
+  get '/' do
     erb :super_hero
   end
 
@@ -15,16 +15,12 @@ class App < Sinatra::Base
     @hero_power = []
     @hero_bio = []
     @team_members = params[:team][:members]
-    @team_members.each do |hero, attrib|
-      @hero_name << attrib[:name]
-      @hero_power << attrib[:power]
-      @hero_bio << attrib[:bio]
+    @team_members.each do |hero|
+      @hero_name << hero[:name]
+      @hero_power << hero[:power]
+      @hero_bio << hero[:bio]
     end
 
     erb :team
   end
-
-
 end
-
-
