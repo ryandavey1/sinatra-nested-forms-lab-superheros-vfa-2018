a9694f1dbbcf155f46c18c6de1911def1cb52adf
Ryan Davey <ryancdavey@icloud.com>
2018-06-29 08:17:15 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,7 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
-
-end
-
-get '/' do
+    get '/' do
     erb :super_hero
   end
 
@@ -26,3 +23,8 @@ get '/' do
 
     erb :team
   end
+
+
+end
+
+
